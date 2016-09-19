aac48a426fa44254ccf26c8378d738f92eddf235
nathanallen <thenathanator@gmail.com>
2016-09-19 11:55:56 -0700

    partial game 1 refactor

--- game-1.js
+++ game-1.js
@@ -1,47 +1,80 @@
 $(document).ready(function() {
 
-  $('.flash').click(function() {
-    $("#box").html('<img src="http://i105.photobucket.com/albums/m219/trexrell/flash_zps15f35f5c.gif" width="auto" height= "50px" margin="auto">')
-  })
-  $('.batman').click(function() {
-    $('#box').html('<img src= "http://www.puppstheories.com/spriterips/BatmanReturns6fWalk.gif" width="auto" height="50px" margin="auto">')
-  })
-  $('.wonderwoman').click(function(){
-    $('#box').html('<img src= "http://mmii.info/icons/lightspeeddash/supers_WonderWomanSprite.gif" width="auto" height="50px" margin= "auto">')
+
+  // Player 1 character options
+  var $character_target = $("#box");
+  var characters = [
+    {
+      $el: $('.flash'),
+      img: "http://i105.photobucket.com/albums/m219/trexrell/flash_zps15f35f5c.gif",
+      name: "Flash"
+    },
+    {
+      $el: $('.batman'),
+      img: "http://www.puppstheories.com/spriterips/BatmanReturns6fWalk.gif",
+      name: "Batman"
+    },
+    {
+      $el: $('.wonderwoman'),
+      img: "http://mmii.info/icons/lightspeeddash/supers_WonderWomanSprite.gif",
+      name: "WonderWoman"
+    }
+  ];
+
+
+  characters.forEach(function(character){
+    character.$el.click(function() {
+      $character_target.html('<img src="' + character.img + '">');
+    })
   })
 
-  $('.rflash').click(function(){
-    $('#square').html('<img src= "http://i.imgur.com/LDWD1.gif" width="auto" height="50px" margin="auto">')
+  // Player 2 character options
+  // TODO: refactor following the pattern above
+  $('.rflash').click(function() {
+    $('#square').html('<img src="http://i.imgur.com/LDWD1.gif">')
   })
-  $('.thejoker').click(function(){
-    $('#square').html('<img src= "http://www.puppstheories.com/spriterips/JokerWalk7fSNES.gif" width="auto" height="50px" margin="auto">')
+  $('.thejoker').click(function() {
+    $('#square').html('<img src="http://www.puppstheories.com/spriterips/JokerWalk7fSNES.gif">')
   })
-  $('.giganta').click(function(){
-    $('#square').html('<img src= "http://img.photobucket.com/albums/v243/deanjo2000/gigantagrow.gif" width="auto" height="50px" margin="auto">')
+  $('.giganta').click(function() {
+    $('#square').html('<img src="http://img.photobucket.com/albums/v243/deanjo2000/gigantagrow.gif">')
   })
 
-function playGame(){
-    var counter1 = 0;
-    var counter2 = 0;
-  $(window).keypress(function(event) {
-    if (event.which === 97) {
-      counter1++;
-      $('.first:nth-child(' + counter1 + ')').html('<img src="http://i105.photobucket.com/albums/m219/trexrell/flash_zps15f35f5c.gif" width="auto" height= "50px" margin:"auto">');
-      if (counter1 === 8) {
-        alert("Player 1 won!")
+  function playGame() {
+    var player1 = {
+      counter: 0,
+      keyCode: 97 // e.g. "a" char
+    };
+
+    var player2 = {
+      counter: 0,
+      keyCode: 108 // e.g. "j" char
+    };
+
+    var players = [player1, player2];
+
+    var winPosition = 42;
+
+    $(window).keypress(function(event) {
+      // TODO: Refactor to be DRY. What if we had 10 players?
+      if (event.which === players[0].keyCode) {
+        players[0].counter++;
+        $('.first:nth-child(' + players[0].counter + ')').html('<img src="http://i105.photobucket.com/albums/m219/trexrell/flash_zps15f35f5c.gif" width="auto" height= "50px" margin:"auto">'); // FIXME: hardcoded image
+        if (players[0].counter === winPosition) {
+          alert("Player 1 won!");
+        }
       }
-    }
-    if (event.which === 108) {
-      counter2++;
-      $('.second:nth-child(' + counter2 + ')').html('<img src="http://i.imgur.com/LDWD1.gif" width="auto" height="50px" margin:"auto">');
-      if (counter2 === 8) {
-        alert("player 2 won!")
+      if (event.which === players[1].keyCode) {
+        players[1].counter++;
+        $('.second:nth-child(' + players[1].counter + ')').html('<img src="http://i.imgur.com/LDWD1.gif" width="auto" height="50px" margin:"auto">'); // FIXME: hardcoded image
+        if (players[1].counter === winPosition) {
+          alert("player 2 won!");
+        }
       }
-    }
-  })
-}
+    });
+  }
 
 
 
   playGame();
-})
+});
